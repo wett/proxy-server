f0f798d1666190ba690de8353bc3b238e6b36c92
Terry Go <gqlpub@gmail.com>
2017-06-22 15:59:11 +0800

    add another kcptun server

--- install_kcptun_go_server.sh
+++ install_kcptun_go_server.sh
@@ -2,4 +2,5 @@
 
 source $HOME/.profile
 
-go get -u github.com/xtaci/kcptun/server
+go get -u github.com/xtaci/kcptun/server  -o kcptun_server1
+go get -u github.com/xtaci/kcptun/server  -o kcptun_server2
